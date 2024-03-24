a9d00d1b4c6fbfd3cad3c5d74d2ce3290fe1abe9
Karsten Zhou <1339688323@qq.com>
2024-03-24 11:00:10 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import "@google/model-viewer";
+import { ref, watch } from "vue";
+import type { Ref } from "vue";
+import type { ModelViewerElement } from "@google/model-viewer";
+import naiveUiThemeOverrides from "./assets/naive-ui-theme-overrides.json";
+import {
+  NConfigProvider,
+  NCard,
+  NRadioGroup,
+  NRadio,
+  NSpace,
+  NSpin,
+} from "naive-ui";
+
+let loading: Ref<boolean> = ref(true);
+let modelViewer: Ref<ModelViewerElement | null> = ref(null);
+let animation: Ref<string> = ref("idle");
+let animations: Ref<string[]> = ref([]);
+// watch modelViewer load
+watch(
+  modelViewer,
+  () => {
+    modelViewer.value?.addEventListener("load", () => {
+      animations.value = modelViewer.value?.availableAnimations || [];
+      loading.value = false;
+      console.log(animations.value);
+    });
+  },
+  { once: true }
+);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <n-config-provider :themeOverrides="naiveUiThemeOverrides">
+    <n-spin :show="loading">
+      <n-card
+        class="h-[calc(100vh-1rem)] w-full"
+        content-class=" flex flex-col"
+      >
+        <model-viewer
+          ref="modelViewer"
+          src="v1.glb"
+          camera-controls
+          shadow-intensity="1"
+          autoplay
+          :animation-name="animation"
+          class="w-full grow"
+        >
+          <div slot="progress-bar"></div>
+        </model-viewer>
+        <div class="w-full border-t-2 pt-2">
+          <n-radio-group v-model:value="animation">
+            <n-space>
+              <n-radio v-for="item in animations" :key="item" :value="item">
+                {{ item }}
+              </n-radio>
+            </n-space>
+          </n-radio-group>
+        </div>
+      </n-card>
+    </n-spin>
+  </n-config-provider>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => {
+            if (tag === "model-viewer") return true;
+            if (tag.slice(0, 7) === "fluent-") return true;
+          },
+        },
+      },
+    }),
+  ],
+  base: "/3d-model/",
+});
