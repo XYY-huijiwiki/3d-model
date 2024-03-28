f6fa9c7c3fd15f456621a1b60464f94f4440cfa9
Karsten Zhou <1339688323@qq.com>
2024-03-28 22:54:51 +0800

    remove js

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +17,11 @@ export default defineConfig({
       },
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   base: "/3d-model/",
   assetsInclude: ["**/*.glb"],
 });
